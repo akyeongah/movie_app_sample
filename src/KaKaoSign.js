f13fd99945f5c90debf7e4379bb7cee7e610f0f8
Dragon0111ga <dragon0111ga@gmail.com>
2021-07-01 02:27:38 +0900

    pogi

--- src/KaKaoSign.js
+++ src/KaKaoSign.js
@@ -6,46 +6,42 @@ class KaKaoSign extends React.Component{
         super(props);
         this.state = {
             data: 'kakao',
-            id: 'default id',
-            name: 'default name'
+            isSignUp: false
         }
         
     }
     componentDidMount(){ 
-        //window.Kakao.init("fcc43e6ec23a4ed3fee1804310e95d59")
+        window.Kakao.init('fcc43e6ec23a4ed3fee1804310e95d59');
     }
-
+    
     logout = (res) => {
         console.log("logout");
         window.Kakao.Auth.logout();
     }
-
+    
     responseKaKao = (res) => {
         console.log("call responseKaKao")
         this.setState({
             data: res,
-            id: res.profile.id,
-            name: res.profile.properties.nickname,
             isSignUp: true
         })
         console.log(res)
     }
-
+    
     responseFail = (err) => {
         alert(err);
     }
-
+    
     render() {
+        
         return (
-           <div>
-                <button onClick={this.logout}>로그아웃</button>
-                <br/><br/><br/>
+            <div>
                 {this.state.isSignUp 
                 ? (
                     <p className="afterLogin">{this.state.name}님 어서오세요.</p>
-                ):(
-                    <KakaoLogin
-                        api-Key={"fcc43e6ec23a4ed3fee1804310e95d59"}
+                    ):(
+                        <KakaoLogin
+                        jsKey={"fcc43e6ec23a4ed3fee1804310e95d59"}
                         onSuccess={this.responseKaKao}
                         onFailure={this.responseFail}
                         getProfile={true}
